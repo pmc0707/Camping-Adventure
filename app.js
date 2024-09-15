228435d4b03edaf8082d0a794915a06f8ecce09b
pmc0707 <pranshugoodboy@gmail.com>
2024-09-15 15:55:29 +0530

    Listing the models

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("../MiniProject/models/listing.js")
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 main().then(()=>{
     console.log("db is connected")
@@ -13,6 +14,20 @@ async  function main() {
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
   });
+  app.get("/testListing", async (req, res) => {
+      let sampleListing = new Listing({
+        title: "My New Villa",
+        description: "By the beach",
+        price: 1200,
+        location: "Calangute, Goa",
+        country: "India",
+      });
+    
+      await sampleListing.save();
+      console.log("sample was saved");
+      res.send("successful testing");
+    });
+    
   app.listen(8080, () => {
     console.log("server is listening to port 8080");
   });  
