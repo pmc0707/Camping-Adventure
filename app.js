01a05289868f0bb3e986b0c80097d2b71ada3ac2
pmc0707 <pranshugoodboy@gmail.com>
2024-09-15 15:11:45 +0530

    set up of project

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ async  function main() {
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
   });
-
   app.listen(8080, () => {
     console.log("server is listening to port 8080");
   });  
