b2856b911db7c5f302991cebb90a66816eda7cb6
pmc0707 <pranshugoodboy@gmail.com>
2024-10-06 10:09:35 +0530

    add detail to front page

--- app.js
+++ app.js
@@ -14,31 +14,46 @@ async  function main() {
 }
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"))
-
+app.use(express.urlencoded({extended: true}))
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
   });
 //index route
-  app.get("/listing", async(req,res) =>{
+  app.get("/listings", async(req,res) =>{
     const allListing =await Listing.find({});
-    res.render("listings/index.ejs",{allListing})
-    
-    
+    res.render("listings/index.ejs",{allListing})   
   })
-  // app.get("/testListing", async (req, res) => {
-  //     let sampleListing = new Listing({
-  //       title: "My New Villa",
-  //       description: "By the beach",
-  //       price: 1200,
-  //       location: "Calangute, Goa",
-  //       country: "India",
-  //     });
-    
-  //     await sampleListing.save();
-  //     console.log("sample was saved");
-  //     res.send("successful testing");
-  //   });
-    
+  //new route
+  app.get("/listings/new",(req,res)=>{
+    res.render("listings/new.ejs")
+  })
+  //show route
+
+  app.get("/listings/:id", async (req, res) => {
+      let { id } = req.params;
+      id = id.trim();
+      if (!mongoose.isValidObjectId(id)) {
+          return res.status(400).send("Invalid listing ID.");
+      }  
+      try {
+          const listing = await Listing.findById(id);
+  
+          if (!listing) {
+              return res.status(404).send("Listing not found.");
+          }  
+          res.render("listings/show.ejs", { listing });
+      } catch (err) {
+          console.error(err);
+          res.status(500).send("Server error while fetching listing.");
+      }
+  });
+  //create route
+  app.post("/listings",async (req,res)=>{
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
+  })
+
   app.listen(8080, () => {
     console.log("server is listening to port 8080");
   });  
