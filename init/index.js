7a16aaff67b3a2fc5c85792a974fb85f9084537e
pmc0707 <pranshugoodboy@gmail.com>
2024-11-21 11:59:36 +0530

    listing owner initialize

--- init/index.js
+++ init/index.js
@@ -18,6 +18,8 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
+  initData.data = initData.data.map((obj)=>({...obj,
+    owner:"673d9eae7650d97c88373591"}))
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
