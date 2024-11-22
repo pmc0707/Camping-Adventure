a83a54ce8a75f38c1ca71fdb2f91a042bf736d25
pmc0707 <pranshugoodboy@gmail.com>
2024-11-22 11:26:46 +0530

    authorize to review

--- middelware.js
+++ middelware.js
@@ -1,48 +1,48 @@
 const Listing = require("./models/listing");
 const ExpressError = require('./utils/ExpressError.js')
-const {listingSchema, reviewSchema} = require("./schema.js")
+const { listingSchema, reviewSchema } = require("./schema.js")
 
-module.exports.isLoggedIn = (req,res,next)=>{
-    if(!req.isAuthenticated()) {
+module.exports.isLoggedIn = (req, res, next) => {
+    if (!req.isAuthenticated()) {
         req.session.redirectUrl = req.originalUrl;
-        req.flash("error","you must be logged in to create listing!");
+        req.flash("error", "you must be logged in to create listing!");
         return res.redirect("/login")
     }
     next();
 }
-module.exports.saveRedirectUrl = ( req, res, next) =>{
-    if(req.session.redirectUrl) {
+module.exports.saveRedirectUrl = (req, res, next) => {
+    if (req.session.redirectUrl) {
         res.locals.redirectUrl = req.session.redirectUrl;
     }
     next();
 };
 
-module.exports.isOwner = async(req,res,next) =>{
-    let {id} = req.params;
+module.exports.isOwner = async (req, res, next) => {
+    let { id } = req.params;
     let listing = await Listing.findById(id);
-        if(!listing.owner.equals(res.locals.currUser._id)){
-            req.flash("error","You are not the owner of this listing");
+    if (!listing.owner.equals(res.locals.currUser._id)) {
+        req.flash("error", "You are not the owner of this listing");
         return res.redirect(`/listings/${id}`);
-        }
+    }
+    next();
+}
+
+module.exports.validateListing = (req, res, next) => {
+    let { error } = listingSchema.validate(req.body);
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(404, errMsg);
+    } else {
         next();
     }
+};
 
-    module.exports.validateListing = (req, res, next) => {
-        let { error } = listingSchema.validate(req.body);
-        if (error) {
-          let errMsg = error.details.map((el) => el.message).join(",");
-          throw new ExpressError(404, errMsg);
-        } else {
-          next();
-        }
-      };
-
-      module.exports.validateReview = (req, res, next) => {
-        let { error } = reviewSchema.validate(req.body);
-        if (error) {
-          let errMsg = error.details.map((el) => el.message).join(",");
-          throw new ExpressError(404, errMsg);
-        } else {
-          next();
-        }
-      };
+module.exports.validateReview = (req, res, next) => {
+    let { error } = reviewSchema.validate(req.body);
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(404, errMsg);
+    } else {
+        next();
+    }
+};
