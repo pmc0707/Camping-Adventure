f30488085e612440c5b482f9a7b131c476e6ccd4
pmc0707 <pranshugoodboy@gmail.com>
2024-11-22 11:30:24 +0530

    authorize review backend

--- routes/review.js
+++ routes/review.js
@@ -4,14 +4,14 @@ const { wrapAsync } = require('../utils/WrapAsync.js');
 const ExpressError = require('../utils/ExpressError.js');
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
-const {validateReview} = require("../middelware.js")
+const {validateReview, isLoggedIn} = require("../middelware.js")
 
 
 //REVIEW post route
-router.post("/", validateReview, wrapAsync(async (req, res) => {
+router.post("/", isLoggedIn, validateReview, wrapAsync(async (req, res) => {
     let listing = await Listing.findById(req.params.id)
     let newReview = new Review(req.body.review);
-
+    newReview.author = req.user._id;
     listing.reviews.push(newReview);
     await newReview.save();
     await listing.save();
