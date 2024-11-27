f13b7b7a7d841915abbce8d47107b70fc6097052
pmc0707 <pranshugoodboy@gmail.com>
2024-11-27 18:45:26 +0530

    add some icons

--- app.js
+++ app.js
@@ -45,9 +45,9 @@ const sessionOptions = {
     httpOnly: true,
   },
 };
-app.get("/", (req, res) => {
-  res.send("Hi, I am root");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hi, I am root");
+// });
 
 app.use(session(sessionOptions));
 app.use(flash());
