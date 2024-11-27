c0534cbb91f93014d673aea7536457750d30f034
pmc0707 <pranshugoodboy@gmail.com>
2024-11-27 18:14:20 +0530

    implementation of when clicked on map ponit it shows message

--- public/js/map.js
+++ public/js/map.js
@@ -3,12 +3,16 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: "map", 
     style: "mapbox://styles/mapbox/streets-v12",
-    center: coordinates,
+    center: listing.geometry.coordinates,
     zoom: 9,
 });
 
-console.log(coordinates)
 
 const marker = new mapboxgl.Marker({color: 'Red'})
-.setLngLat(coordinates)
+.setLngLat(listing.geometry.coordinates)
+.setPopup(
+    new mapboxgl.Popup({offset:25}).setHTML(
+        `<h4>${listing.location}</h4><p>Exact location provided after booking</p>`
+    )
+)
 .addTo(map);
